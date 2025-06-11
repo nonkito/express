61153791210a12c320886b91f531bda42bec496b
nonkito <aon.iaos310@gmail.com>
2025-06-12 04:00:44 +0900

    back to 8-1

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
 
 //３．Expressオブジェクトの作成と基本設定
 var app = express();
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
